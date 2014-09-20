0d34ebe8e253adfe48cbee1881053ca1e0e9f214
Tomek Bawej <tomek.bawej@gmail.com>
2014-09-20 10:30:28 +0200

    wandersync

--- @P3Summary/P3SummaryMerge.m
+++ @P3Summary/P3SummaryMerge.m
@@ -1,2 +1,5 @@
 function summary = P3SummaryMerge(p3sa, p3sb)
+    functions=[p3sa.functions, p3sb.functions];
+    H=[p3sa.confusionMatrix, p3sb.confusionMatrix];
+    summary=P3Summary(functions, H);
 endfunction;
