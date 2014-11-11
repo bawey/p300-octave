2bac9f854d98ea8bbc9351a9bcd442ee03022c5d
Tomek Bawej <tomek.bawej@gmail.com>
2014-11-11 23:41:16 +0100

    more train-test facilities for c++ interface

---
+++ P3Toolkit/askClassifier.m
@@ -0,0 +1,10 @@
+function [] = askClassifier(classifier, p3session, tr_mean, tr_std)
+    for(periodNo=1:p3session.periodsCount)
+        [feats, labels, stimuli] = classificationData(p3session, [1:p3session.channelsCount], [periodNo]);
+        %labels may well be empty here! this one is for online predictions mainly!
+        feats=centerTestData(feats, tr_mean, tr_std);
+        [preds probs] = classify(classifier, feats);
+        [sign row column labelodds] = periodCharacterPrediction(stimuli, probs);
+        printf('period %d: %d. row, %d. column. Seems like character %s \n', periodNo, row, column, sign);
+    endfor;
+endfunction;
--- P3Toolkit/trainClassifier.m
+++ P3Toolkit/trainClassifier.m
@@ -1,4 +1,8 @@
 %
-function model = trainClassifier(funcHandle, funcParaCell, trainSession)
+function [model, tr_mean, tr_std] = trainClassifier(funcHandle, funcParaCell, trainSession)
+    [tfeats, tlabels, tstimuli] = classificationData(trainSession);
+    [tfeats, tr_mean, tr_std] = centerTrainData(tfeats);
+    
+    model = feval(funcHandle, tfeats, tlabels, funcParaCell);
     
 endfunction;
