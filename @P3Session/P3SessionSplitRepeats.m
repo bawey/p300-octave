0f710c55bcee69526a4ef0f31784488719446aca
Tomasz Bawej <tomek.bawej@gmail.com>
2014-11-25 17:18:18 +0100

    stupid splitting bug fix

--- @P3Session/P3SessionSplitRepeats.m
+++ @P3Session/P3SessionSplitRepeats.m
@@ -4,16 +4,16 @@
 % arg: split_ratio determines how many output periods will be produced from each original one
 %   split_ratio can either be 'max' to yield max(factor(epochsCountPerPeriod)) periods or 'min'
 function p3s = P3SessionSplitRepeats(session, split_ratio='min')
-    factor = min(factor(session.epochsCountPerPeriod));
+    thefactor = min(factor(session.epochsCountPerStimulus));
     if(strcmp(split_ratio,'max'))
-        factor = max(factor(session.epochsCountPerPeriod));
+        thefactor = max(factor(session.epochsCountPerStimulus));
     endif;
     newtargets=[];
     
     oldtargets=session.targets;
     
     for(trg=1:session.periodsCount)
-        newtargets = [newtargets; repmat(oldtargets(trg,:),factor, 1)];
+        newtargets = [newtargets; repmat(oldtargets(trg,:),thefactor, 1)];
     endfor;
     
     p3s = P3Session(session.signal, session.stimuli, newtargets, session.channelsCount, session.samplingRate, session.channelNames);
