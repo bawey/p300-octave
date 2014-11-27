c89fd7dd7d9bb54427d9a157d6607ef441d6c270
Tomek Bawej <tomek.bawej@gmail.com>
2014-11-27 12:59:19 +0100

    demo same day slightly altered

--- EpocDemos/demoSameDayPeroformance.m
+++ EpocDemos/demoSameDayPeroformance.m
@@ -17,14 +17,14 @@ eeg_dir = '~/Desktop/eeg/';
 classification_methods = 'all';
 balancing = 'no';
 reps_split = 'no';
-xsplit = '3';
+xsplit = 5;
 
 % BUCKET 1
 b1_p3_tr = downsample(P3SessionMerge(P3SessionLobenotion(eeg_dir,'tomek_session_011'), P3SessionLobenotion(eeg_dir,'tomek_session_009')),6);
 b1_p3_te = downsample(P3SessionLobenotion(eeg_dir,'tomek_session_012'),6);
 
 [b1_model b1_modelCell b1_featsSelectCell b1_summary] = pickClassifier(b1_p3_tr, classification_methods, reps_split, balancing, xsplit);
-b1_scores = trainTestMesh(b1_p3_tr, b1_p3_te, b1_modelCell);
+b1_scores = trainTestMesh(b1_p3_tr, b1_p3_te, b1_modelCell)
 
 
 % BUCKET 2
@@ -32,7 +32,7 @@ b2_p3_tr = downsample(P3SessionMerge(P3SessionLobenotion(eeg_dir,'tomek_session_
 b2_p3_te = downsample(P3SessionLobenotion(eeg_dir,'tomek_session_014'),6);
 
 [b2_model b2_modelCell b2_featsSelectCell b2_summary] = pickClassifier(b2_p3_tr, classification_methods, reps_split, balancing, 5);
-b2_scores = trainTestMesh(b2_p3_tr, b2_p3_te, b2_modelCell);
+b2_scores = trainTestMesh(b2_p3_tr, b2_p3_te, b2_modelCell)
 
 
 % BUCKET 3
@@ -43,7 +43,7 @@ b3_p3_tr = downsample(P3SessionMerge(
             ),6);
 
 [b3_model b3_modelCell b3_featsSelectCell b3_summary] = pickClassifier(b3_p3_tr, classification_methods, reps_split, balancing, xsplit);
-b3_scores = trainTestMesh(b3_p3_tr, b3_p3_te, b3_modelCell);
+b3_scores = trainTestMesh(b3_p3_tr, b3_p3_te, b3_modelCell)
 
 % BUCKET 4
 b4_p3_tr = P3SessionMerge(downsample(P3SessionLobenotion(eeg_dir,'tomek_session_017'),6), b3_p3_tr);
@@ -53,7 +53,7 @@ b4_p3_tr = P3SessionMerge(downsample(P3SessionLobenotion(eeg_dir,'tomek_session_
 b4_p3_tr = P3SessionMerge(downsample(P3SessionLobenotion(eeg_dir,'tomek_session_024'),6), b4_p3_tr);
 
 [b4_model b4_modelCell b4_featsSelectCell b4_summary] = pickClassifier(b4_p3_tr, classification_methods, reps_split, balancing, xsplit);
-b4_scores = trainTestMesh(b3_p3_tr, b3_p3_te, b3_modelCell);
+b4_scores = trainTestMesh(b4_p3_tr, b3_p3_te, b4_modelCell)
 
 %save all the variables into a binary file
 save('-binary', sprintf('%s/demoSameDayPerformance_reps_not_split.oct', eeg_dir));
