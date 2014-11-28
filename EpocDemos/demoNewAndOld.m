995e79ab9d11afe1f1ac411ddf0b9c5313100865
Tomek Bawej <tomek.bawej@gmail.com>
2014-11-28 13:27:56 +0100

    demoNewAndOld.m changed to include b3 vs b1 testing and confidence reporting

--- EpocDemos/demoNewAndOld.m
+++ EpocDemos/demoNewAndOld.m
@@ -3,9 +3,6 @@ eeg_file_stem = 'tomek_session_';
 %% AVAILABLE SESSIONS:
 
 % some issues happened when recording the first phrase. let's try the other way round
-%b1_trains = [10, 21];
-%b1_tests = [13, 20, 22, 23, 24];
-
 b1_trains = [13, 21, 22];
 b1_tests = [10, 20, 23, 24];
 
@@ -38,15 +35,34 @@ for(b = {'b1', 'b2', 'b3'})
     %b2 will reuse the model found in b1
     if(strcmp(b, 'b2')==false)
         [model modelCell featsSelectCell summary] = pickClassifier(p3tr, 'all', 'no', 'no', 10);
-        scores=trainTestMesh(p3tr, p3te, modelCell)
+        [scores, confidence] = trainTestMesh(p3tr, p3te, modelCell)
     
         results.(sprintf('%s_scores', b))=scores;
         results.(sprintf('%s_model', b))=model;
         results.(sprintf('%s_modelCell', b))=modelCell;
         results.(sprintf('%s_summary', b))=summary;
+        results.(sprintf('%s_confidence', b))=confidence;
     else
-        scores=trainTestMesh(p3tr, p3te, results.b1_modelCell)
+        [scores, confidence] = trainTestMesh(p3tr, p3te, results.b1_modelCell)
         results.(sprintf('%s_scores', b))=scores;
+        results.(sprintf('%s_confidence', b))=confidence;
     endif;
     
 endfor;
+
+% extra part: use the accumulated classifier against test data from first point
+
+p3tr=P3SessionLobenotion(eeg_dir, sprintf('%s%03d', eeg_file_stem, b3_trains(1)));
+for(i=b3_trains(2:end))
+    p3tr = P3SessionMerge(p3tr, P3SessionLobenotion(eeg_dir, sprintf('%s%03d', eeg_file_stem, i)));
+endfor;
+    
+p3te=P3SessionLobenotion(eeg_dir, sprintf('%s%03d', eeg_file_stem, b1_tests(1)));
+    for(i=b1_tests(2:end))
+    p3te = P3SessionMerge(p3te, P3SessionLobenotion(eeg_dir, sprintf('%s%03d', eeg_file_stem, i)));
+endfor;
+
+[results.b3_b1_scores, results_b3_b1_confidence] = trainTestMesh(p3tr, p3te, results.b3_modelCell);
+
+% save all the results
+save('-binary', sprintf('%s/demoNewOld.oct', eeg_dir), results);
