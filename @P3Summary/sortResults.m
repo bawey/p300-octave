46ebbcf7c5193120d7b78c9da3b0b9c9501e1386
Tomek Bawej <tomek.bawej@gmail.com>
2014-12-01 02:22:49 +0100

    just the sorting for now

--- @P3Summary/sortResults.m
+++ @P3Summary/sortResults.m
@@ -8,26 +8,26 @@ function [coords, value] = sortResults(p3summary, mode='naive')
             for(z=1:length(summary{x}{y}))
                 [info, stats]=confusionMatrixInfo(summary{x}{y}{z}.naive);
 %               printf('feats computation: %15s, feats selection: %20s, classifier (naive): %10s , %s', fc{x}, fs{y}, cl{z}, info);
-                scoreboard.naive=[scoreboard.naive; stats.f1*0.01, x, y, z];
+                  scoreboard.naive=[scoreboard.naive; stats.f1*0.00000000001, x, y, z];
 
                 [info, stats]=confusionMatrixInfo(summary{x}{y}{z}.aware);
 %               printf('feats computation: %15s, feats selection: %20s, classifier (aware): %10s , %s', fc{x}, fs{y}, cl{z}, info);
-                scoreboard.aware=[scoreboard.aware; stats.f1*0.01, x, y, z];
+                  scoreboard.aware=[scoreboard.aware; stats.f1*0.00000000001, x, y, z];
 
                 if(isfield(summary{x}{y}{z}, 'correctSymbols'))
-                    scoreboard.naive(end, 1)+=summary{x}{y}{z}.correctSymbols;
+%                      scoreboard.naive(end, 1)+=summary{x}{y}{z}.correctSymbols;
                 endif;
                 
                  if(isfield(summary{x}{y}{z},'fewestRepeats'))
-                    scoreboard.naive(end, 1)+=(1/summary{x}{y}{z}.fewestRepeats);
+%                      scoreboard.naive(end, 1)+=(1/summary{x}{y}{z}.fewestRepeats);
                 endif;
                 
                 if(isfield(summary{x}{y}{z}, 'msme') && ~isnan(summary{x}{y}{z}.msme))
-                      scoreboard.naive(end, 1)+=(summary{x}{y}{z}.msme * -0.000001);
+                      scoreboard.naive(end, 1)+=1/(summary{x}{y}{z}.msme);
                 endif;
 
                 if(isfield(summary{x}{y}{z}, 'mse') && ~isnan(summary{x}{y}{z}.msme))
-                      scoreboard.naive(end, 1)+=(summary{x}{y}{z}.mse * -0.0000000001);
+                      scoreboard.naive(end, 1)+=1/(summary{x}{y}{z}.mse);
                 endif;
 
             endfor;
